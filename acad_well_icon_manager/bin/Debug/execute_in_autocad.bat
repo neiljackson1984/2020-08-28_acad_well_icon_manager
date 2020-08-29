bad0d56cd9bfc0d71a0080e3587dfb36e3cfb1fb
Neil Jackson <neil@rattnow.com>
2020-08-29 13:51:38 -0700

    copied and modified the objectARX folder in order to create a hopefully-working project

--- acad_well_icon_manager/bin/Debug/execute_in_autocad.bat
+++
@@ -1,1 +0,0 @@
-autohotkey "C:\work\2020-08-28_acad_well_icon_manager\acad_well_icon_manager\executeInAutocad.ahk" "C:\work\2020-08-28_acad_well_icon_manager\acad_well_icon_manager\bin\Debug\acad_well_icon_manager.dll" 
